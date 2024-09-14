41621b3d43a9d4edf3168be3cb7fa3eeadafebdf
Hiba-Mohamed <hibamoha987@gmail.com>
2024-09-14 12:13:18 -0230

    generate low case pass working with defalut length of 8

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ const process = require("node:process");
 const userArguments = process.argv.slice(2);
 console.log("");
 console.log("");
-console.log("_____________________________________________________________________");
-console.log("|                Welcome to Your Password Generator                 |");
-console.log("|                For more info, run: npx pass-gen -h                |");
-console.log("---------------------------------------------------------------------");
+console.log(
+  "_____________________________________________________________________"
+);
+console.log(
+  "|                Welcome to Your Password Generator                 |"
+);
+console.log(
+  "|                For more info, run: npx pass-gen -h                |"
+);
+console.log(
+  "---------------------------------------------------------------------"
+);
 
 // console.log(userArguments);
 const firstUserArg = userArguments[0];
 const firstUserArgNumber = parseInt(firstUserArg);
+
+const createLowerCasePass = (length = 8) => {
+  const alphabetsLower = "abcdefghijklmnopqrstuvwxyz";
+  let lowCasePass = ""
+  for (i = 0; i < length; i++) {
+    const randomLetterIndex = Math.floor(Math.random() * 26);
+    const randomLetter = alphabetsLower[randomLetterIndex]
+    lowCasePass +=randomLetter
+  }
+  console.log(alphabetsLower.length);
+  console.log(lowCasePass)
+};
+
 if (userArguments.length === 1 && isNaN(firstUserArgNumber)) {
   console.log("                                                           ");
-  console.log(
-    `    Error      : Invalid entry, you enetered ${firstUserArg}  `
-  );
+  console.log(`    Error      : Invalid entry, you enetered ${firstUserArg}  `);
   console.log("                 as the characters length of your password.");
   console.log(
     "    Quick fix  : Please enter a valid number after 'pass-gen' command."
@@ -24,7 +43,7 @@ if (userArguments.length === 1 && isNaN(firstUserArgNumber)) {
   console.log("");
 } else {
   if (userArguments.length === 0) {
-    userArguments[0] = 8;
+    createLowerCasePass();
   } else {
     if (userArguments[0] === "-h") {
       console.log(
